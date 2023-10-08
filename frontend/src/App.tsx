20b721a1d9902f20d13b4886d60f1a410077646a
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-08 10:59:31 +0200

    feat: frontend has access to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import axios from 'axios';
 
 export default function App() {
+	const [helloText, setHelloText] = useState<string>("");
+
+    useEffect(loadHelloText, []);
+
+    function loadHelloText (){
+        axios.get("/api/hello")
+            .then((response) => {
+                if (response.status!==200)
+                    throw "Get wrong response status, when loading Hello text: "+response.status;
+				setHelloText(response.data);
+            })
+            .catch((error)=>{
+                console.error(error);
+            })
+    }
 
 	return (
 		<>
 			<h1>Rick & Morty API - Viewer</h1>
+			Response: {helloText}
 		</>
 	)
 }
